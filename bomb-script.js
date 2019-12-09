89b7b67a83f1d7c9b52693d609f0c5df3ce9815d
KJFreys1 <kfreyermuth20@gmail.com>
2019-12-09 13:48:56 -0800

    added controls to move player if not moving into stone

--- bomb-script.js
+++ bomb-script.js
@@ -1,38 +1,42 @@
 const gameBoard = document.querySelector('#game-board')
-let boxesRow = []
 let boxes = []
 let stone = []
 let player
-let playerIndex = 16
+let direction
+let xPos = 1
+let yPos = 1
 
 createBoard()
 
 function createBoard () {
     for (let row = 0; row < 15; row++) {
-        boxesRow = []
+        let stoneRow = []
+        let boxesRow = []
         for (let col = 0; col < 15; col++) {
             let newBox = document.createElement('div')
             newBox.classList.add('box')
             if (row == 0 || row == 14) {
                 newBox.classList.add('stone')
-                stone.push([row, col])
+                stoneRow.push(true)
             } else if (col == 0 || col == 14) {
                 newBox.classList.add('stone')
-                stone.push([row, col])
+                stoneRow.push(true)
             } else if (row % 2 == 0 && col % 2 == 0) {
                 newBox.classList.add('stone')
-                stone.push([row, col])
+                stoneRow.push(true)
             } else {
                 newBox.classList.add('grass')
+                stoneRow.push(false)
             }
             gameBoard.appendChild(newBox)
             boxesRow.push(newBox)
         }
         boxes.push(boxesRow)
+        stone.push(stoneRow)
     }
+    player = boxes[yPos][xPos]
+    player.style.backgroundColor = 'white'
 }
-player = boxes[1][1]
-player.style.backgroundColor = 'white'
 
 document.addEventListener('keydown', evt => {
     if (evt.code == 'KeyW' || evt.code == 'KeyA' || evt.code == 'KeyS' || evt.code == 'KeyD') {
@@ -42,5 +46,28 @@ document.addEventListener('keydown', evt => {
 })
 
 function movePlayer () {
-
+    boxes[yPos][xPos].style.backgroundColor = 'green'
+    if (direction == 'w') {
+        if (stone[yPos-1][xPos] == false) {
+            yPos--
+        }
+    } else if (direction == 's') {
+        if (stone[yPos+1][xPos] == false) {
+            yPos++        }
+    } else if(direction == 'a') {
+        if (stone[yPos][xPos-1] == false) {
+            xPos--
+        }
+    } else if (direction == 'd') {
+        if (stone[yPos][xPos+1] == false) {
+            xPos++
+        }
+    }
+    player = boxes[yPos][xPos]
+    player.style.backgroundColor = 'white'
 }
+
+document.addEventListener('keypress', evt => {
+    direction = evt.key
+    movePlayer()
+})
