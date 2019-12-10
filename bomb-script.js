e2ab526f97f14d4122417b1955d491ab99475c6f
KJFreys1 <kfreyermuth20@gmail.com>
2019-12-10 09:55:33 -0800

    added randomly generated bricks to map and (broken) bomb class

--- bomb-script.js
+++ bomb-script.js
@@ -1,12 +1,82 @@
 const gameBoard = document.querySelector('#game-board')
 let boxes = []
 let stone = []
+let brick = []
+let willBrick
 let player
+let playerBombs = [true, true]
+let playerBombIndex = [0, 1]
 let direction
 let xPos = 1
 let yPos = 1
 let bombX
 let bombY
+
+class Bomb {
+    constructor (index, x, y) {
+        this.index = index
+        this.bombX = x
+        this.bombY = y
+        this.fire = []
+    }
+
+    placeBomb() {
+        boxes[this.bombY][this.bombX].classList.add('bomb-black')
+        console.log(this.bombX)
+    }
+
+    bombRed () {
+        console.log(this.bombX)
+        boxes[this.bombY][this.bombX].classList.remove('bomb-black')
+        boxes[this.bombY][this.bombX].classList.add('bomb-red')
+    }
+    
+    bombBlack () {
+        boxes[this.bombY][this.bombX].classList.remove('bomb-red')
+        boxes[this.bombY][this.bombX].classList.add('bomb-black')
+    }
+
+    bombExplode () {
+        boxes[this.bombY][this.bombX].classList.remove('bomb-black')
+        this.fire.push(boxes[this.bombY][this.bombX])
+        if (stone[this.bombY-1][this.bombX] == false) {
+            this.fire.push(boxes[this.bombY-1][this.bombX])
+            if (stone[this.bombY-2][this.bombX] == false) {
+                this.fire.push(boxes[this.bombY-2][this.bombX])
+            }
+        } if (stone[this.bombY+1][this.bombX] == false) {
+            this.fire.push(boxes[this.bombY+1][this.bombX])
+            if (stone[this.bombY+2][this.bombX] == false) {
+                this.fire.push(boxes[this.bombY+2][this.bombX])
+            }
+        } if (stone[this.bombY][this.bombX+1] == false) {
+            this.fire.push(boxes[this.bombY][this.bombX+1])
+            if (stone[this.bombY][this.bombX+2] == false) {
+                this.fire.push(boxes[this.bombY][this.bombX+2])
+            }
+        } if (stone[this.bombY][this.bombX-1] == false) {
+            this.fire.push(boxes[this.bombY][this.bombX-1])
+            if (stone[this.bombY][this.bombX-2] == false) {
+                this.fire.push(boxes[this.bombY][this.bombX-2])
+            }
+        }
+        this.addFire
+        setTimeout(this.removeFire, 800)
+        
+    }
+    
+    addFire () {
+        for (let f = 0; f < this.fire.length; f++) {
+            this.fire.classList.add('fire')
+        }
+    }
+
+    removeFire () {
+        for (let f = 0; f < this.fire.length; f++) {
+            this.fire.classList.remove('fire')
+        }
+    }
+}
 
 createBoard()
 
@@ -14,9 +84,13 @@ function createBoard () {
     for (let row = 0; row < 15; row++) {
         let stoneRow = []
         let boxesRow = []
+        let bricksRow = []
         for (let col = 0; col < 15; col++) {
+            willBrick = Math.ceil(Math.random() * 10)
             let newBox = document.createElement('div')
             newBox.classList.add('box')
+
+            //generate stone
             if (row == 0 || row == 14) {
                 newBox.classList.add('stone')
                 stoneRow.push(true)
@@ -30,40 +104,69 @@ function createBoard () {
                 newBox.classList.add('grass')
                 stoneRow.push(false)
             }
+
+            //generate bricks
+            if (row > 0 && row < 14 && col > 0 && col < 14) {
+                if (willBrick < 10) {
+                    if ((row == 1 || row == 13) && col > 3 && col < 11) {
+                        newBox.classList.remove('grass')
+                        newBox.classList.add('brick')
+                        bricksRow.push(true)
+                    } else if ((row == 2 || row == 3) && col > 1 && col < 13) {
+                        if (!stoneRow[col]) {
+                            newBox.classList.remove('grass')
+                            newBox.classList.add('brick')
+                            bricksRow.push(true)
+                        }
+                    } else if ((row == 11 || row == 12) && col > 1 && col < 13) {
+                        if (!stoneRow[col]) {
+                            newBox.classList.remove('grass')
+                            newBox.classList.add('brick')
+                            bricksRow.push(true)
+                        }
+                    } else if (row > 3 && row < 11 && col > 0 && col < 14) {
+                        if (!stoneRow[col]) {
+                            newBox.classList.remove('grass')
+                            newBox.classList.add('brick')
+                            bricksRow.push(true)
+                        }
+                    } else {
+                        bricksRow.push(false)
+                    }
+                } else {
+                    bricksRow.push(false)
+                }
+            } else {
+                bricksRow.push(false)
+            }
             gameBoard.appendChild(newBox)
             boxesRow.push(newBox)
         }
         boxes.push(boxesRow)
         stone.push(stoneRow)
+        brick.push(bricksRow)
     }
     player = boxes[yPos][xPos]
-    
 }
+
 let img = document.createElement('img')
 img.src = 'sprites/white-sprite/white_down/white_standing1.png'
 player.appendChild(img)
 
-document.addEventListener('keydown', evt => {
-    if (evt.code == 'KeyW' || evt.code == 'KeyA' || evt.code == 'KeyS' || evt.code == 'KeyD') {
-        direction = evt.code
-        movePlayer()
-    }
-})
-
 function movePlayer () {
     if (direction == 'w') {
-        if (stone[yPos-1][xPos] == false) {
+        if (!stone[yPos-1][xPos] && !brick[yPos-1][xPos]) {
             yPos--
         }
     } else if (direction == 's') {
-        if (stone[yPos+1][xPos] == false) {
+        if (!stone[yPos+1][xPos] && !brick[yPos+1][xPos]) {
             yPos++        }
     } else if(direction == 'a') {
-        if (stone[yPos][xPos-1] == false) {
+        if (!stone[yPos][xPos-1] && !brick[yPos][xPos-1]) {
             xPos--
         }
     } else if (direction == 'd') {
-        if (stone[yPos][xPos+1] == false) {
+        if (!stone[yPos][xPos+1] && !brick[yPos][xPos+1]) {
             xPos++
         }
     }
@@ -78,61 +181,12 @@ document.addEventListener('keypress', evt => {
         movePlayer()
     }
     if (evt.code == 'Space') {
-        placeBomb()
+        let newBomb = new Bomb(1, xPos, yPos)
+        newBomb.placeBomb()
+        setTimeout(newBomb.bombRed, 500)
+        setTimeout(newBomb.bombBlack, 1000)
+        setTimeout(newBomb.bombRed, 1500)
+        setTimeout(newBomb.bombBlack, 2000)
+        setTimeout(newBomb.bombExplode, 2500)
     }
 })
-
-function placeBomb () {
-    bombX = xPos
-    bombY = yPos
-    boxes[bombY][bombX].classList.add('bomb-black')
-    setTimeout(bombRed, 500)
-    setTimeout(bombBlack, 1000)
-    setTimeout(bombRed, 1500)
-    setTimeout(bombBlack, 2000)
-    setTimeout(bombExplode, 2500)
-}
-
-function bombRed () {
-    boxes[bombY][bombX].classList.remove('bomb-black')
-    boxes[bombY][bombX].classList.add('bomb-red')
-}
-
-function bombBlack () {
-    boxes[bombY][bombX].classList.remove('bomb-red')
-    boxes[bombY][bombX].classList.add('bomb-black')
-}
-
-function bombExplode () {
-    boxes[bombY][bombX].classList.remove('bomb-black')
-    boxes[bombY][bombX].classList.add('fire')
-    if (stone[bombY-1][bombX] == false) {
-        boxes[bombY-1][bombX].classList.add('fire')
-        if (stone[bombY-2][bombX] == false) {
-            boxes[bombY-2][bombX].classList.add('fire')
-        }
-    } if (stone[bombY+1][bombX] == false) {
-        boxes[bombY+1][bombX].classList.add('fire')
-        if (stone[bombY+2][bombX] == false) {
-            boxes[bombY+2][bombX].classList.add('fire')
-        }
-    } if (stone[bombY][bombX+1] == false) {
-        boxes[bombY][bombX+1].classList.add('fire')
-        if (stone[bombY][bombX+2] == false) {
-            boxes[bombY][bombX+2].classList.add('fire')
-        }
-    } if (stone[bombY][bombX-1] == false) {
-        boxes[bombY][bombX-1].classList.add('fire')
-        if (stone[bombY][bombX-2] == false) {
-            boxes[bombY][bombX-2].classList.add('fire')
-        }
-    }
-    setTimeout(removeFire, 800)
-}
-
-function removeFire (y, x) {
-    let fire = document.querySelectorAll('.fire')
-    for (let j = 0; j < fire.length; j++) {
-        fire[j].classList.remove('fire')
-    }
-}
